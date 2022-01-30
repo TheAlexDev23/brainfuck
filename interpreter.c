dfd305cebca920c514db2f5ba799d5cc352c6d65
TheAlexDev23 <thealexdev23@gmail.com>
2022-01-30 14:13:34 +0100

    added comments

--- interpreter.c
+++ interpreter.c
@@ -1,26 +1,34 @@
 #include <stdio.h>
 #include <string.h>
 
+//Maximum allowed loops 
 #define MAX_LOOPS 500
 
+//Brainfuck code that would be executed
 char*code = " ++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.";
-char*reader;
-int started_loops = 0;
-char*loops[MAX_LOOPS];
 
+char*reader; // A pointer used to read values from code
+
+int started_loops = 0; //A counter that would help us indicate the last started loop
+char*loops[MAX_LOOPS]; // Array of pointers pointing to the begining of all loops 
+
+//The main array
 char array[30000] = {0}; 
-char *ptr = array;
+char *ptr = array; //Current node in the array 
 
 void calculation();
 
 int main(void) {
-    reader = code; 
+    reader = code; //inuitialize our reader to start reading from the begining of the code 
+    
+    //check if we have reached the end of the brainfuck code
     while(reader - code < strlen(code)) {
-        ++reader;
-        calculation();
+        ++reader; //move readre forwards one value
+        calculation(); //do the needed calculations in the current brainfuck symbol
     }
 }
 
+//Do the needed calculations with each character of the code
 void calculation() {
     switch(*reader){
         case '>':
@@ -46,9 +54,10 @@ void calculation() {
             loops[started_loops] = reader; 
             break;
         case ']':
+            //if end of loop is reached go to the last begining of loop if *ptr != 0
             if(*ptr){
                reader = loops[started_loops];
-            } else {
+            } else { // make the last loop the previous to the current
                 started_loops--;
             } 
             break;
